2a678214a8cfaad6910404c23015cce44bcd823b
lightfor <death_light@foxmail.com>
2017-06-09 23:30:02 +0800

    regex test

---
+++ src/main/java/org/lightfor/basic/RegexTest.java
@@ -0,0 +1,12 @@
+package org.lightfor.basic;
+
+/**
+ * regex test
+ * Created by Light on 2017/6/8.
+ */
+public class RegexTest {
+
+    public static void main(String[] args) {
+        System.out.println("a\nb\nc".split("\\n").length);
+    }
+}
