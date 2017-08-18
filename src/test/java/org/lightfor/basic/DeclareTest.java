c31dc898d9db2c456a9a153dd76934844b0e6663
lightfor <death_light@foxmail.com>
2017-08-18 21:30:56 +0800

    declare test

---
+++ src/test/java/org/lightfor/basic/DeclareTest.java
@@ -0,0 +1,22 @@
+package org.lightfor.basic;
+
+/**
+ * declare test
+ * Created by Light on 2017/8/18.
+ */
+public class DeclareTest {
+
+    public void test(){
+        int _a;
+        int $a;
+        //number start
+        //int 1a;
+
+
+        //keywords
+        //int void;
+
+
+
+    }
+}
