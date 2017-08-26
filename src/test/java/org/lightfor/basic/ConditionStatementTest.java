97d6eb52fe5f183a78d5ad6a9b955cdb7e9ebb1b
lightfor <death_light@foxmail.com>
2017-08-26 23:26:28 +0800

    condition statement test

---
+++ src/test/java/org/lightfor/basic/ConditionStatementTest.java
@@ -0,0 +1,58 @@
+package org.lightfor.basic;
+
+import org.junit.Test;
+
+/**
+ * condition statement test
+ * Created by Light on 2017/8/26.
+ */
+public class ConditionStatementTest {
+
+    @Test
+    public void test(){
+        long start = System.currentTimeMillis();
+        for(int i = 0 ; i < 1000000000 ; i++){
+            if(i == 300000000){
+                System.out.println(1);
+            } else if(i == 700000000){
+                System.out.println(2);
+            } else {
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+
+    @Test
+    //switch slower than if
+    public void test1(){
+        long start = System.currentTimeMillis();
+        for(int i = 0 ; i < 1000000000 ; i++){
+            switch (i){
+                case 300000000:
+                    System.out.println(1);
+                    break;
+                case 700000000:
+                    System.out.println(2);
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    @Test
+    public void test2(){
+        long start = System.currentTimeMillis();
+        for(int i = 0 ; i < 1000000000 ; i++){
+            if(i == 300000000){
+                System.out.println(1);
+            }
+            if(i == 700000000){
+                System.out.println(2);
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
