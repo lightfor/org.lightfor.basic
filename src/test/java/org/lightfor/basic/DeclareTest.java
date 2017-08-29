c81d12780621de24f1eb52d0c3584e81f38816be
lightfor <death_light@foxmail.com>
2017-08-29 17:15:07 +0800

    add ambiguous declare

--- src/test/java/org/lightfor/basic/DeclareTest.java
+++ src/test/java/org/lightfor/basic/DeclareTest.java
@@ -9,14 +9,50 @@ public class DeclareTest {
     public void test(){
         int _a;
         int $a;
-        //number start
+        //error, number start
         //int 1a;
 
 
-        //keywords
+        //error, keywords
         //int void;
 
 
+
+    }
+
+    public void test1(){
+        class A {
+            void a(){
+
+            }
+
+            //error, same method name and parameter signature（number, order and type）
+            //ambiguous when call a();
+/*            int a(){
+
+            }*/
+
+            void a(int i, float f){
+
+            }
+
+            void a(float f, int i){
+
+            }
+
+            void a(int i){
+
+            }
+
+            void a(float f){
+
+            }
+        }
+        A a = new A();
+        a.a(9.0f,5);
+        //error, ambiguous
+        //a.a(9,5);
+        a.a(9);
 
     }
 }
