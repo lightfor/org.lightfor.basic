2b19a161b212ec41b4a37f36ccee60e46a08400d
lightfor <death_light@foxmail.com>
2017-11-17 17:49:40 +0800

    add some thread examples

--- src/test/java/org/lightfor/basic/ThreadTest.java
+++ src/test/java/org/lightfor/basic/ThreadTest.java
@@ -2,6 +2,7 @@ package org.lightfor.basic;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -40,4 +41,121 @@ public class ThreadTest {
     public static void main(String[] args) {
         new ThreadTest().test4();// maintain running
     }
+
+    //wait notify can apply to consumer-producer model
+    @Test
+    public void test5() {
+        class ThreadB extends Thread{
+            private int total;
+
+            @Override
+            public void run(){
+                synchronized(this){
+                    for(int i=0; i < 10 ;  i++){
+                        total += i;
+                    }
+                    notify();
+                }
+
+            }
+        }
+
+        ThreadB thread = new ThreadB();
+        thread.start();
+
+        synchronized (thread) {
+            try{
+                System.out.println("Waiting for b to complete...");
+                thread.wait();
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(thread.total);
+
+    }
+
+    @Test
+    public void test6(){
+        class ThreadB extends Thread{
+            private int total;
+
+            @Override
+            public void run(){
+                for(int i=0; i < 10 ;  i++){
+                    total += i;
+                }
+            }
+        }
+
+        ThreadB thread = new ThreadB();
+        thread.start();
+
+        System.out.println(thread.total);
+    }
+
+    //2000 or smaller than 2000, sometimes ArrayIndexOutOfBoundsException, not thread safe
+    @Test
+    public void test7() throws InterruptedException {
+        class ThreadSB extends Thread{
+            private StringBuilder sb;
+            private CountDownLatch countDownLatch;
+
+            private ThreadSB(StringBuilder sb, CountDownLatch countDownLatch){
+                this.sb = sb;
+                this.countDownLatch = countDownLatch;
+            }
+
+            @Override
+            public void run(){
+                for(int i = 0 ; i < 1000; i++){
+                    sb.append(" ");
+                }
+                countDownLatch.countDown();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        ThreadSB a = new ThreadSB(sb, countDownLatch);
+        ThreadSB b = new ThreadSB(sb, countDownLatch);
+        a.start();
+        b.start();
+
+        countDownLatch.await();
+        System.out.println(sb.length());
+    }
+
+    //always 2000, thread safe
+    @Test
+    public void test8() throws InterruptedException {
+        class ThreadSB extends Thread{
+            private StringBuffer sb;
+            private CountDownLatch countDownLatch;
+            private ThreadSB(StringBuffer sb, CountDownLatch countDownLatch){
+                this.sb = sb;
+                this.countDownLatch = countDownLatch;
+            }
+
+            @Override
+            public void run(){
+                for(int i = 0 ; i < 10000; i++){
+                    sb.append(" ");
+                }
+                countDownLatch.countDown();
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        ThreadSB a = new ThreadSB(sb, countDownLatch);
+        ThreadSB b = new ThreadSB(sb, countDownLatch);
+        a.start();
+        b.start();
+
+        countDownLatch.await();
+        System.out.println(sb.length());
+    }
+
 }
