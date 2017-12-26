91fcb6ba95aaea73a8aae2f55ae4baf4a51c61a1
lightfor <death_light@foxmail.com>
2017-12-26 11:40:51 +0800

    add a example

--- src/test/java/org/lightfor/basic/NullTest.java
+++ src/test/java/org/lightfor/basic/NullTest.java
@@ -8,4 +8,10 @@ public class NullTest {
     public void test1(){
         System.out.println("1".equals(null));
     }
+
+    @Test
+    public void test2() {
+        Integer a = null;
+        System.out.println(a == 1);//((Integer)a).intValue() == 1
+    }
 }
