2f3d288e7c7a246029d48d1b0364b3801078cee6
lightfor <death_light@foxmail.com>
2018-01-28 22:48:25 +0800

    field access

--- src/test/java/org/lightfor/basic/InheritanceTest.java
+++ src/test/java/org/lightfor/basic/InheritanceTest.java
@@ -107,4 +107,38 @@ public class InheritanceTest {
     interface IC extends IA, IB{
     }
 
+
+    @Test
+    public void test1(){
+        class A {
+            int i=1;
+
+            private int j = 1;
+            void a(){
+                System.out.println("A");
+            }
+        }
+
+        class B extends A {
+            int i=2;
+
+            private String j = "2";
+            void a() {
+                System.out.println("B");
+            }
+        }
+
+        A a = new A();
+        A b = new B();
+        a.a();
+        b.a();
+        System.out.println(a.i);
+        System.out.println(b.i);
+        System.out.println(((B) b).i);
+
+        System.out.println(a.j);
+        System.out.println(b.j);
+        System.out.println(((B) b).j);
+    }
+
 }
