b2025335688aec59c98e64875045d6fd47975dbb
lightfor <death_light@foxmail.com>
2018-01-29 17:53:01 +0800

    LinkedHashMap test

--- src/test/java/org/lightfor/basic/MapTest.java
+++ src/test/java/org/lightfor/basic/MapTest.java
@@ -3,6 +3,7 @@ package org.lightfor.basic;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapTest {
@@ -42,7 +43,7 @@ public class MapTest {
 
             @Override
             public boolean equals(Object var1) {
-                return value.equals(((A)var1).value);
+                return var1 instanceof A && value.equals(((A) var1).value);
             }
 
             @Override
@@ -57,5 +58,25 @@ public class MapTest {
         A a1 = new A("A");
         map.put(a1, "D");
         System.out.println(map.get(a));
+    }
+
+    @Test
+    public void test3() {
+        LinkedHashMap<String,String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("2", "B");
+        linkedHashMap.put("1", "A");
+        for(Map.Entry<String,String> entry : linkedHashMap.entrySet()){
+            System.out.println(entry.getKey());
+        }
+    }
+
+    @Test
+    public void test4() {
+        Map<String,String> map = new HashMap<>();
+        map.put("2", "B");
+        map.put("1", "A");
+        for(Map.Entry<String,String> entry : map.entrySet()){
+            System.out.println(entry.getKey());
+        }
     }
 }
